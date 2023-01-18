5d6186ba87f5ac13eb5df1874e959da9c486902e
lawrence2807 <lawrencect28@gmail.com>
2023-01-18 09:59:54 -0600

    Creación del proyecto

---
+++ src/main/java/comTienda_IQ2023/TiendaIq2023Application.java
@@ -0,0 +1,13 @@
+package comTienda_IQ2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaIq2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaIq2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/comTienda_IQ2023/TiendaIq2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package comTienda_IQ2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaIq2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
